450e9ee2f40af55cd5bfdf609a9520b0c1113e51
Flottegurke <justus.dicker@web.de>
2025-05-18 19:29:01 +0200

    add updateDotfiles.sh; remove setup.sh

--- setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# System files
-sudo stow --target=/etc ly
-
---
+++ updateDotfiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo stow hyprland
+sudo stow kitty
+sudo stow starship
+sudo stow bat 
+sudo stow --target=/etc ly
+sudo stow waybar
