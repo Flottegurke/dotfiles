7b92ce518a8bb3686ac0116ecb90ff76be0bd075
Flottegurke <justus.dicker@web.de>
2025-05-26 02:26:18 +0200

    improove ocrClipboard

--- hyprland/hypr/scripts/ocrClipboard.sh
+++ hyprland/hypr/scripts/ocrClipboard.sh
@@ -4,12 +4,21 @@ IMG="ocrshot.png"
 HOME="/tmp"
 FULL="/tmp/ocrshot.png"
 
-# Take screenshot and check if it was successful
-hyprshot -m region -o "$HOME" -f "$IMG" -s;
+
+hyprshot -m region -o "$HOME" -f "$IMG" -s; # take screenshot
+
+# Wait for the screenshot file to be created (max 5 seconds)
+TIMEOUT=5 # in sec
+WAITED=0
+while [ ! -f "$FULL" ] && [ $WAITED -lt $TIMEOUT ]; do
+    sleep 0.1
+    WAITED=$(echo "$WAITED + 0.2" | bc)
+done 
+
+
 # If the screenshot was saved, run OCR
 if [ -f "$FULL" ]; then
     tesseract "$FULL" - -l eng+deu+fra --psm 1 | wl-copy
 else
-    notify-send "❌ OCR Failed" "Screenshot file not found."
+    notify-send "❌ OCR Failed" "Screenshot file not found after waiting  ${WAITED} seconds."
 fi
-
