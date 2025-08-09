160923c0b0163d727c677c14c96b069d9df2f903
Flottegurke <justus.dicker@web.de>
2025-08-09 04:15:43 +0200

    refactor no-update-dedection logic

--- waybar/hypr/scripts/getOutOfDatePackages.sh
+++ waybar/hypr/scripts/getOutOfDatePackages.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
+# If this script is executed by waybar, the local package database can not get synced (no sudo permission). 
+# It is therfore nessesary for the updateDotfiles.sh file to chreat a sudo cronjob that does this every 2 hours.
 repo_updates=$(checkupdates 2>/dev/null)
 aur_updates=$(yay -Qua)
 
 updates="$repo_updates"$'\n'"$aur_updates"
 count=$(echo "$updates" | grep -c .)
 
-# Fallback for when no updates are available
+tooltip=""
 if [ "$count" -eq 0 ]; then
-  echo "{\"text\": \" 0\", \"tooltip\": \"No updates available\"}"
-  exit 0
+  tooltip="No updates available"
+else
+  tooltip=$(printf "%s" "$count updates available:\n$updates" | sed ':a;N;$!ba;s/\n/\\n/g')
 fi
-# Escape newlines for JSON tooltip
-tooltip_text="$count updates available:\n$updates"
-tooltip_escaped=$(printf "%s" "$tooltip_text" | sed ':a;N;$!ba;s/\n/\\n/g')
 
 # Output JSON for Waybar
-echo "{\"text\": \"  $count\", \"tooltip\": \"$tooltip_escaped\"}"
+echo "{\"text\": \"  $count\", \"tooltip\": \"$tooltip\"}"
