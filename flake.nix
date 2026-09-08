73f9b5d1db643dc8db450e326637c74c3eea3dc4
flottegurke <justus.dicker@web.de>
2026-09-09 01:39:20 +0200

    add basic flake config
    - add flake.nix configuration
    - add flake.lock lockfile

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Flottegurkes NixOS config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "x86_64-linux" "aarch64-linux" ];
+
+    imports = [
+      ./parts/nixos-hosts.nix
+      ./parts/packages.nix
+    ];
+
+  };
+}
