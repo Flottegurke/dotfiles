5f0169edbf026818dc3326c8dbc6153eedd80ae0
flottegurke <justus.dicker@web.de>
2026-09-10 23:51:51 +0200

    only inject agenix if it is used

--- parts/packages.nix
+++ parts/packages.nix
@@ -2,10 +2,16 @@
 {
   perSystem = { pkgs, system, ... }: # for each system, run this function with the system specific arguments
   let
-    mkPackage = name: pkgs.callPackage ../pkgs/${name} { # provide dependencies for <name> package
-      agenix = inputs.agenix.packages.${system}.default;
-    }; # add overrides
     pkgNames = builtins.attrNames (pkgs.lib.filterAttrs (_: type: type == "directory") (builtins.readDir ../pkgs)); # contains name of every directory in `../pkgs`
+    mkPackage = name:
+      let
+        fn = import ../pkgs/${name};
+        wantsAgenix = pkgs.lib.hasAttr "agenix" (builtins.functionArgs fn);
+        extraArgs = pkgs.lib.optionalAttrs wantsAgenix {
+          agenix = inputs.agenix.packages.${system}.default;
+        };
+      in
+      pkgs.callPackage ../pkgs/${name} extraArgs; # provide dependencies for <name> package
   in
   {
     packages = pkgs.lib.genAttrs pkgNames mkPackage; # autodiscover every custom module and provide their dependencies
