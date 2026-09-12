25d165ac7ac160dfe931c69c25e5e09fb47f153f
flottegurke <justus.dicker@web.de>
2026-09-12 21:00:40 +0200

    add `libvirtd` group

--- users/flottegurke/default.nix
+++ users/flottegurke/default.nix
@@ -2,7 +2,7 @@
 {
   description = "Justus Dicker";
   # the `users-<username>-password` secret is used as the user password
-  extraGroups = [ "wheel" "networkmanager" "audio" "video" "docker" ];
+  extraGroups = [ "wheel" "networkmanager" "audio" "video" "docker" "libvirtd" ];
   isNormalUser = true;
   group = "users";
   shell = pkgs.bashInteractive;
